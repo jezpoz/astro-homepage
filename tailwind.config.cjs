93ce4444451c313ab0730eef45c7d57cdc0f5c0f
jezpoz <jesfor94@gmail.com>
2022-11-10 18:36:41 +0100

    WIP, linux subsystem is too slow to work with

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'web3-blue': '#30CFD0',
+				'web3-pink': '#c43ad6',
+			},
+		},
 	},
 	plugins: [],
 }
